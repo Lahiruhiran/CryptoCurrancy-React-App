56d440d0fbe089b747af5fb612cca3617feee13b
Lahiru Wijesooriya <lahiruhiranwijesooriyansbm@gmail.com>
2021-09-20 19:29:10 +0530

    Add ant style for app and Routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 import {Switch, Route, Link} from 'react-router-dom';
 import {Layout, Typography,Space} from 'antd';
 
-import { Navbar } from "./componets";
+import { Navbar, HomePage,Exchanges, Cryptocurrencies, CryptoDetails, News } from "./componets";
 import './App.css'
+
 const App = () => {
     return (
         <div className="app">
@@ -11,7 +12,28 @@ const App = () => {
                <Navbar />
             </div>
                <div className="main">
+                        <Layout>
+                        <div className="routes">
+                            <Switch>
+                                <Route exact path="/">
+                                    <HomePage />
+                                </Route>
+                                <Route exact path="/exchanges">
+                                    <Exchanges />
+                                </Route>
+                                <Route exact path="/cryptocurrencies">
+                                    <Cryptocurrencies />
+                                </Route>
+                                <Route exact path="/crypto/:coinId">
+                                    <CryptoDetails />
+                                </Route>
+                                <Route exact path="/news">
+                                    <News />
+                                </Route>
+                            </Switch>
 
+                        </div>
+                            </Layout>
                </div>
             <div className="footer">
 
